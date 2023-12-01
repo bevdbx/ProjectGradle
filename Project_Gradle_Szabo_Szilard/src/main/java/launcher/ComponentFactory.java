bdf943584a24f79281942c59e9a63f2c1eb4af60
bevdbx <bevdbx607@gmail.com>
2023-12-01 02:41:02 +0200

    Added Singleton for application dependencies

---
+++ Project_Gradle_Szabo_Szilard/src/main/java/launcher/ComponentFactory.java
@@ -0,0 +1,71 @@
+package launcher;
+
+import controller.LoginController;
+import database.DatabaseConnectionFactory;
+import javafx.stage.Stage;
+import repository.book.BookRepositoryMySQL;
+import repository.security.RightsRolesRepository;
+import repository.security.RightsRolesRepositoryMySQL;
+import repository.user.UserRepository;
+import repository.user.UserRepositoryMySQL;
+import service.book.BookService;
+import service.book.BookServiceImpl;
+import service.user.AuthenticationService;
+import service.user.AuthenticationServiceMySQL;
+import view.LoginView;
+
+import java.sql.Connection;
+
+public class ComponentFactory {
+    private final LoginView loginView;
+    private final LoginController loginController;
+    private final AuthenticationService authenticationService;
+    private final UserRepository userRepository;
+    private final RightsRolesRepository rightsRolesRepository;
+    private final BookRepositoryMySQL bookRepository;
+    private final BookService bookService;
+    private static ComponentFactory instance;
+
+    public static ComponentFactory getInstance(Boolean componentsForTests, Stage stage) {
+        if(instance == null) {
+            instance = new ComponentFactory(componentsForTests, stage);
+        }
+
+        return instance;
+    }
+
+    public ComponentFactory(Boolean componentsForTests, Stage stage) {
+        Connection connection = DatabaseConnectionFactory.getConnectionWrapper(componentsForTests).getConnection();
+        this.rightsRolesRepository = new RightsRolesRepositoryMySQL(connection);
+        this.userRepository = new UserRepositoryMySQL(connection,rightsRolesRepository);
+        this.authenticationService = new AuthenticationServiceMySQL(userRepository,rightsRolesRepository);
+        this.loginView = new LoginView(stage);
+        this.loginController = new LoginController(loginView,authenticationService);
+        this.bookRepository = new BookRepositoryMySQL(connection);
+        this.bookService = new BookServiceImpl(bookRepository);
+    }
+
+    public AuthenticationService getAuthenticationService() {
+        return authenticationService;
+    }
+
+    public UserRepository getUserRepository() {
+        return userRepository;
+    }
+
+    public RightsRolesRepository getRightsRolesRepository() {
+        return rightsRolesRepository;
+    }
+
+    public LoginView getLoginView() {
+        return loginView;
+    }
+
+    public BookRepositoryMySQL getBookRepository() {
+        return bookRepository;
+    }
+
+    public LoginController getLoginController() {
+        return loginController;
+    }
+}
